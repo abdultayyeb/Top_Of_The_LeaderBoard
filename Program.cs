5f1e011f4e3435c48b590a7b69d726aac88580b3
abdultayyeb <abdulbasariya@gmail.com>
2022-05-06 22:08:17 -0800

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfPlayers = Convert.ToInt32(Console.ReadLine());
+
+            List<int> scores = new List<int>();
+            int count = 0;
+            while (count<numOfPlayers)
+            {
+                int score = Convert.ToInt32(Console.ReadLine());
+                //your code goes here
+                scores.Add(score);
+                count++;
+
+            }
+
+            //sort the list and output elements
+            List<int> sortScore = new List<int>();
+            scores.Sort();
+            Console.WriteLine(string.Join(" ", scores));
+
+        }
+    }
+}
